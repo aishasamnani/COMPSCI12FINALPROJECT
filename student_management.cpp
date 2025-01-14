57fdf9ebde5bc43b1e3f7ffb3d546512a8532747
Aisha Samnani <83725883+aishasamnani@users.noreply.github.com>
2025-01-14 13:25:34 -0500

    Add files via upload

---
+++ student_management.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Calendar {
+  
+};
+
+class Journal {
+
+};
+
+class Todo {
+
+};
+
+class Pass {
+
+};
+
+int main() {
+    
+    
+
+
+    return 0;
+}
